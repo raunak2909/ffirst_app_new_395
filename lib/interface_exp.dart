d6f1b0e04d044ac343723520984aa7425c3a13d4
raunakpurohit <raunak.wscubetech@gmail.com>
2025-05-16 23:15:16 +0530

    Interface and Mixin

---
+++ lib/interface_exp.dart
@@ -0,0 +1,115 @@
+abstract class Car{
+  accelerate(String speed){
+    print("Accelerating to $speed KM/hr..");
+  }
+
+  brake(){
+    print("Applied brakes!!");
+  }
+
+  changeGear(){
+    print("Changing gear..");
+  }
+
+  String average();
+
+  String price();
+
+
+}
+
+abstract class Audi implements Car{
+
+}
+
+class AudiA8 extends Audi{
+
+  @override
+  String price() {
+    return "1 Crore";
+  }
+
+  @override
+  String average() {
+    return "20 KM/hr";
+  }
+
+  @override
+  accelerate(String speed) {
+    // TODO: implement accelerate
+    throw UnimplementedError();
+  }
+
+  @override
+  brake() {
+    // TODO: implement brake
+    throw UnimplementedError();
+  }
+
+  @override
+  changeGear() {
+    // TODO: implement changeGear
+    throw UnimplementedError();
+  }
+}
+
+class AudiQ7 extends Audi{
+  @override
+  String average() {
+    return "15 KM/hr";
+  }
+
+  @override
+  String price() {
+    return "1.5 Crore";
+  }
+
+  @override
+  accelerate(String speed) {
+    // TODO: implement accelerate
+    throw UnimplementedError();
+  }
+
+  @override
+  brake() {
+    // TODO: implement brake
+    throw UnimplementedError();
+  }
+
+  @override
+  changeGear() {
+    // TODO: implement changeGear
+    throw UnimplementedError();
+  }
+}
+
+class BMWi8 implements Car{
+  @override
+  String average() {
+    return "10 KM/hr";
+  }
+
+  @override
+  String price() {
+    return "1.2 Crore";
+  }
+
+  @override
+  accelerate(String speed) {
+    // TODO: implement accelerate
+    throw UnimplementedError();
+  }
+
+  @override
+  brake() {
+    // TODO: implement brake
+    throw UnimplementedError();
+  }
+
+  @override
+  changeGear() {
+    // TODO: implement changeGear
+    throw UnimplementedError();
+  }
+
+}
---
+++ lib/mixin_exp.dart
@@ -0,0 +1,121 @@
+///int
+///String
+///if
+///else
+///else if
+///for
+///while
+///void
+///return
+///class
+///extends
+///implements
+///with
+///null
+///this
+///super
+///bool
+///true
+///false
+///mixin
+///
+///
+///
+///
+///
+///
+///
+///
+/// final
+/// const
+/// static
+/// in
+/// as
+/// is
+/// switch
+/// case
+/// continue
+/// break
+/// import
+/// export
+///
+///
+///
+///
+/// async
+/// await
+/// try
+/// catch
+/// finally
+/// future
+/// stream
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void main(){
+
+  Calculate calc = Calculate();
+
+  print(calc.add(5, 6));
+  print(calc.sub(5, 6));
+  print(calc.mul(5, 6));
+  print(calc.div(5, 6));
+
+}
+
+mixin Calculations{
+  int add(int no1, int no2){
+    return (no1+no2)*(no1+no2);
+  }
+}
+
+mixin Operations{
+
+  int add(int no1, int no2){
+    return no1+no2;
+  }
+
+  int sub(int no1, int no2){
+    return no1-no2;
+  }
+
+  int mul(int no1, int no2){
+    return no1*no2;
+  }
+
+  num div(int no1, int no2){
+    return no1/no2;
+  }
+
+}
+
+class Calculate with Calculations,Operations{
+
+
+
+}
